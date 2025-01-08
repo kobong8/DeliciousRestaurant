74856d7ee85b80605531cf7e9e9d38ea57d828f9
Kobong <83524779+meticulousdev@users.noreply.github.com>
2025-01-08 21:25:34 +0900

    new class Sraper

--- sample/scraper.py
+++ sample/scraper.py
@@ -3,75 +3,49 @@ import requests
 from typing import List
 import re
 
-# regex 01(example) : [?|&](\w+)=([^\w+])
-# regex 02(replace) : (\w+)=([^\w+])
-# regex 03-1 : (\w+)=(\w+)&?
-# regex 03-2 : "$1": "$2", \n
+class Scraper:
+    def __init__(self):
+        self.__url = "https://category.gmarket.co.kr/listview/L100000002.aspx"
+        self.__headers = {
+            "user-agent": "'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0'"
+        }
 
-## Error
-# 200 OK
-# 401 Unauthorized
-# 403 Forbidden
-# 404 Not Found
-# 418 I’m a teapot
+    def do(self):
+        response = requests.get(self.__url, headers=self.__headers)
+        print(f"response status code: {response.status_code}")
 
-# url = "https://www.coupang.com/np/campaigns/82/components/317679?"
-#
-# params = {
-#     "listSize": "60",
-#     "isPriceRange": "false",
-#     "channel": "user",
-#     "fromComponent": "Y",
-#     "sorter": "bestAsc",
-#     "component": "317679",
-#     "rating": "0"
-# }
-url = "https://category.gmarket.co.kr/listview/L100000002.aspx"
+        soup = BeautifulSoup(response.text, "html.parser")
+        list_box = soup.find("div", class_="plus-goods")
+        item_boxes = list_box.find_all("li")
 
-# web termial : navigator.userAgent
-headers = {
-    "user-agent": "'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0'"
-}
+        result: List = []
+        for item_box in item_boxes:
+            # item 이름
+            item_title = item_box.find("div", class_="img").findChild("a").find("img")["alt"]
 
-# response = requests.get(url, params=params, headers=headers)
-response = requests.get(url, headers=headers)
-print(f"response status code: {response.status_code}")
-soup = BeautifulSoup(response.text, "html.parser")
-list_box = soup.find("div", class_="plus-goods")
-# print(list_box)
-item_boxes = list_box.find_all("li")
-# print(item_boxes)
+            # item 링크
+            item_link = item_box.find("div", class_="img").findChild("a").attrs["href"]
+            pattern = r"https?://[^\s,']+"
+            item_link = re.findall(pattern, item_link)[0]
 
-result: List = []
-for item_box in item_boxes:
-    # item 이름
-    item_title = item_box.find("div", class_="img").findChild("a").find("img")["alt"]
-    print(item_title)
+            # item 가격
+            item_price = item_box.find("div", class_="price").text
 
-    # item 링크
-    # TODO link 추출 필요
-    # item_link = item_box.find("div", class_="img").findChild("a")["href"]
-    item_link = item_box.find("div", class_="img").findChild("a").attrs["href"]
-    pattern = r"https?://[^\s,']+"
-    item_link = re.findall(pattern, item_link)[0]
-    print(item_link)
+            # item 이미지
+            item_img = item_box.find("div", class_="img").findChild("a").find("img")["src"]
 
-    # item 가격
-    item_price = item_box.find("div", class_="price").text
-    print(item_price)
-    
-    # item 이미지
-    item_img = item_box.find("div", class_="img").findChild("a").find("img")["src"]
-    print(item_img)
-    print()
+            item = {
+                "name": item_title,
+                "url": item_link,
+                "price": item_price,
+                "image": item_img
+            }
 
-    item = {
-        "name": item_title,
-        "url": item_link,
-        "price": item_price,
-        "image": item_img
-    }
+            result.append(item)
 
-    result.append(item)
+        return result
 
-print(item)
+if __name__ == "__main__":
+    scraper = Scraper()
+    items = scraper.do()
+    print(items)
