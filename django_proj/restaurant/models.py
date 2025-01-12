14e466f8b5db0ea8abd24a8a70abdecd481e3ae8
Kobong <83524779+meticulousdev@users.noreply.github.com>
2025-01-12 13:05:10 +0900

    model Restarurant

--- django_proj/restaurant/models.py
+++ django_proj/restaurant/models.py
@@ -3,3 +3,9 @@ from django.db import models
 class Restaurant(models.Model):
     name = models.CharField(max_length=50)
     address = models.CharField(max_length=255)
+    Phone = models.CharField(max_length=20)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    rating = models.SmallIntegerField()
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
