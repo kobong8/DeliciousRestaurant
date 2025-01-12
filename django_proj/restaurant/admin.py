34745015417e2fb789c7bc7ee643137572c3e7d7
Kobong <83524779+meticulousdev@users.noreply.github.com>
2025-01-12 16:26:57 +0900

    admin and models update

--- django_proj/restaurant/admin.py
+++ django_proj/restaurant/admin.py
@@ -3,6 +3,5 @@ from .models import Restaurant
 
 class RestaurantAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'phone', 'created_at', 'updated_at']
-    fields = ['name', 'phone']
 
 admin.site.register(Restaurant, RestaurantAdmin)
--- django_proj/restaurant/models.py
+++ django_proj/restaurant/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
 class Restaurant(models.Model):
-    name = models.CharField(max_length=50)
-    address = models.CharField(max_length=255)
-    phone = models.CharField(max_length=20)
+    name = models.CharField("이름", max_length=50, db_index=True)
+    category = models.CharField("카테고리", max_length=50, blank=True, null=True)
+    address = models.CharField("주소", max_length=255)
+    phone = models.CharField("전화번호", max_length=16, help_text="E.164 포맷", db_index=True)
+    image = models.ImageField(upload_to='restaurants/', null=True, blank=True)
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
     rating = models.SmallIntegerField()
