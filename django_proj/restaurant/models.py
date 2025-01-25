cb314caf489afcb2954c24b694ea59dba6144e15
kobong8 <fblsj90@gmail.com>
2025-01-25 10:50:02 +0900

    additional model - RestaurantCategory

--- django_proj/restaurant/models.py
+++ django_proj/restaurant/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
+class RestaurantCategory(models.Model):
+    name = models.CharField("이름", max_length=20)
+
+    class Meta:
+        verbose_name = "가게 카테고리"
+        verbose_name_plural = "가게 카테고리"
+
+    def __str__(self):
+        return self.name
+
 class Restaurant(models.Model):
     name = models.CharField("이름", max_length=50, db_index=True)
     category = models.CharField("카테고리", max_length=50, blank=True, null=True)
