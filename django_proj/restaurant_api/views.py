6af25620dc37ca2ab7bde1777933c057cc75107d
kobong8 <fblsj90@gmail.com>
2025-01-27 10:09:14 +0900

    views - first commit

---
+++ django_proj/restaurant_api/views.py
@@ -0,0 +1,9 @@
+from rest_framework import viewsets
+
+from .serializers import RestaurantSerializer
+from ..restaurant.models import Restaurant
+
+
+class RestaurantViewSet(viewsets.ModelViewSet):
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
