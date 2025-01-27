56024cc5f83e20b287e91dddc5ed6d8389c5bc3e
kobong8 <fblsj90@gmail.com>
2025-01-27 11:05:55 +0900

    Update settings.py
    settings - rest_framework

--- django_proj/proj/settings.py
+++ django_proj/proj/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'restaurant.apps.RestaurantConfig',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
