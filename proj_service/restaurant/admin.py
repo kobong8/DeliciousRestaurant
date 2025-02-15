4c0a5f0a2bf4f502ecc172e351e90d5bd89ce655
kobong8 <fblsj90@gmail.com>
2025-02-15 09:37:34 +0900

    new admin

--- proj_service/restaurant/admin.py
+++ proj_service/restaurant/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Article
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "title",
+        "show_at_index",
+        "is_published",
+        "created_at",
+        "modified_at",
+    ]
+    fields = ["title", "preview_image", "content", "show_at_index", "is_published"]
+    search_fields = ["title"]
+    list_filter = ["show_at_index", "is_published"]
+    date_hierarchy = "created_at"
+    actions = ["make_published"]
