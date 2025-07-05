281cef828a3d5a2eba9a0639b06517fe66eb5b4e
Kobong <fblsj90@gmail.com>
2025-07-06 01:05:34 +0900

    sql_app crud error solved

--- fastapi/sql_example/sql_app/main.py
+++ fastapi/sql_example/sql_app/main.py
@@ -1,8 +1,14 @@
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
-# crud 변경 시 오류 발생
-from . import crud
+from .crud import (
+    get_user_by_email,
+    create_user,
+    get_user,
+    get_users,
+    create_user_item,
+    get_items,
+)
 from .models import Base
 from .schemas import User, UserCreate, Item, ItemCreate
 from .database import SessionLocal, engine
@@ -21,23 +27,24 @@ def get_db():
         db.close()
 
 
+# DONE 함수명과 crud에서 불러오는 명칭이 같아서 오류 발생
 @app.post("/users/", response_model=User)
-def create_user(user: UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, email=user.email)
+def create_new_user(user: UserCreate, db: Session = Depends(get_db)):
+    db_user = get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.create_user(db=db, user=user)
+    return create_user(db=db, user=user)
 
 
 @app.get("/users/", response_model=list[User])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = crud.get_users(db, skip=skip, limit=limit)
+    users = get_users(db, skip=skip, limit=limit)
     return users
 
 
 @app.get("/users/{user_id}", response_model=User)
 def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
+    db_user = get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
@@ -45,10 +52,10 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
 
 @app.post("/users/{user_id}/items/", response_model=Item)
 def create_item_for_user(user_id: int, item: ItemCreate, db: Session = Depends(get_db)):
-    return crud.create_user_item(db=db, item=item, user_id=user_id)
+    return create_user_item(db=db, item=item, user_id=user_id)
 
 
 @app.get("/items/", response_model=list[Item])
 def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    items = crud.get_items(db, skip=skip, limit=limit)
+    items = get_items(db, skip=skip, limit=limit)
     return items
